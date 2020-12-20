338ab851dff4c649adfe743bfb82c15da327cc84
Rikki Prince <rfp@ecs.soton.ac.uk>
2020-12-20 14:01:11 -0800

    Day 4 Part 2 done, in methods

--- 2020/4/count_valid_passports.rb
+++ 2020/4/count_valid_passports.rb
@@ -16,15 +16,73 @@ end
 
 def are_fields_present(passport)
   ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].all? { |field|
-    passport.has_key?(field)
+    if !passport.has_key?(field) then
+      # puts "missing #{field}"
+      return false
+    end
+    true
   }
+end
+
+def height_is_valid(passport)
+  m = /([[:digit:]]+)(cm|in)/.match(passport["hgt"])
+  return false if !m
+  units = m[2]
+  height = m[1].to_i
+  if units == "cm" then
+    return height.between?(150,193)
+  elsif units == "in" then
+    return height.between?(59,76)
+  else
+    # puts "height is invalid"
+    return false
+  end
+end
+
+def year_is_valid(passport, key, start_year, end_year)
+  return false if passport[key].nil?
+  if passport[key] =~ /^[[:digit:]]{4}$/ && passport[key].to_i.between?(start_year,end_year) then
+    return true
+  else
+    # puts "#{key} invalid: #{passport[key]}"
+    return false
+  end
+end
+
+def hair_colour_is_valid(passport)
+  passport["hcl"] =~ /^#[[:xdigit:]]{6}$/
+end
+
+def eye_colour_is_valid(passport)
+  ["amb","blu","brn","gry","grn","hzl","oth"].include?(passport["ecl"])
+end
+
+def passport_id_is_valid(passport)
+  passport["pid"] =~ /^[[:digit:]]{9}$/
+end
+
+def data_is_valid(passport)
+  return (year_is_valid(passport, "byr", 1920,2002) &&
+          year_is_valid(passport, "iyr", 2010,2020) &&
+          year_is_valid(passport, "eyr", 2020,2030) &&
+          height_is_valid(passport) &&
+          hair_colour_is_valid(passport) &&
+          eye_colour_is_valid(passport) &&
+          passport_id_is_valid(passport)
+  )
 end
 
 # Load all lines into list
 lines = File.readlines("input.txt")
 passports = get_hash_from_input(lines)
-number_of_valid_passports = passports.filter { |passport|
+puts "Total: #{passports.count()}"
+
+number_of_passports_with_correct_fields = passports.filter { |passport|
   are_fields_present(passport)
 }.count()
+puts "Correct fields present: #{number_of_passports_with_correct_fields}"
 
-puts number_of_valid_passports
+number_of_passports_with_valid_data = passports.filter { |passport|
+  are_fields_present(passport) && data_is_valid(passport)
+}.count()
+puts "Valid: #{number_of_passports_with_valid_data}"
