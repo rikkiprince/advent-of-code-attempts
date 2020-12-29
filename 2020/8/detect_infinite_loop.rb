11a96ef036892eec425b023af8aed2f0f53e04c8
Rikki Prince <rfp@ecs.soton.ac.uk>
2020-12-29 14:06:36 -0800

    Refactor to solve Day 8 Part 2

--- 2020/8/detect_infinite_loop.rb
+++ 2020/8/detect_infinite_loop.rb
@@ -1,10 +1,15 @@
 require 'Set'
 
+class InfiniteLoop < StandardError
+end
+
 def detect_infinite_loop(operations)
   seen = Set.new
   pc = 0
   accumulator = 0
-  until seen.include?(pc) do
+  until pc >= operations.length do
+    raise InfiniteLoop.new "Operation #{pc} already executed. Accumulator: #{accumulator}" if seen.include?(pc)
+
     seen << pc
     m = /(?<op>acc|jmp|nop) (?<arg>[+-][0-9]+)/.match operations[pc]
     operation = m[:op]
@@ -24,5 +29,26 @@ def detect_infinite_loop(operations)
 end
 
 # Load all lines into list
-operations = File.readlines("input.txt")
-puts "Accumulator immediately before infinite loop: #{detect_infinite_loop(operations)}"
+operations = File.readlines("example.txt")
+# puts "{Part 1: Accumulator immediately before infinite loop: #{detect_infinite_loop(operations)}"
+
+# Part 2
+# TODO: Would prefer to do this with filter_map but don't have Ruby 2.7
+potential = operations.map.with_index { |operation,index| index if operation.match(/nop|jmp/) }.compact
+potential.each do |op_index|
+  temp_operations = operations.clone.map(&:clone)
+  case temp_operations[op_index][0..2]
+    when "nop"
+      temp_operations[op_index].gsub!("nop", "jmp")
+    when "jmp"
+      temp_operations[op_index].gsub!("jmp", "nop")
+  end
+
+  begin
+    puts "Accumulator at end: #{detect_infinite_loop(temp_operations)}"
+    break
+  rescue InfiniteLoop => e
+    puts "[Infinite Loop] #{e.message}"
+  end
+  puts
+end
