39f18b3fa3253a71d0e157c4eef6345818b5468c
Rikki Prince <rfp@ecs.soton.ac.uk>
2021-12-23 00:16:56 -0800

    Solve Day 13 for input

---
+++ 2021/13/part1.rb
@@ -0,0 +1,59 @@
+require 'pp'
+
+class String
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def bold;           "\e[1m#{self}\e[22m" end
+end
+
+def plot(dots)
+  max_x = (dots.max {|a,b| a["x"] <=> b["x"]})["x"] + 1
+  max_y = (dots.max {|a,b| a["y"] <=> b["y"]})["y"] + 1
+  map = Array.new(max_y) {Array.new(max_x," ")}
+  dots.each do |d|
+    puts "#{d["x"]},#{d["y"]}. #{map.length}"
+    map[d["y"]][d["x"]] = "#"
+  end
+
+  max_y.times do |y|
+    max_x.times do |x|
+      print map[y][x].red
+    end
+    puts
+  end
+end
+
+def calculate(file_name)
+  lines = File.readlines(file_name)
+
+  dots = []
+  folds = []
+  lines.each do |line|
+    case line
+    when /([0-9]+),([0-9]+)/
+      dots << {"x"=>$1.to_i,"y"=>$2.to_i}
+    when /fold along ([xy])=([0-9]+)/
+      folds << [$1,$2.to_i]
+    end
+  end
+
+  folds.each do |fold|
+    axis = fold[0]
+    position = fold[1]
+    to_fold,stay_still = dots.partition {|d| d[axis] > position }
+
+    puts "Position: #{position}, Axis: #{axis}"
+    to_fold.map! do |d|
+      d[axis] -= 2*(d[axis]-position)
+      d
+    end
+    dots = to_fold | stay_still
+  end
+  pp dots.length
+
+  plot(dots)
+end
+
+calculate("example_input.txt")
+calculate("input.txt")
