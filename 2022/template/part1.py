8a6495145689935774fa93aad57a2a01d5db8dd8
Rikki Prince <rfp@ecs.soton.ac.uk>
2022-12-01 22:35:50 -0700

    fixup! Template and script for Python

--- 2022/template/part1.py
+++ 2022/template/part1.py
@@ -1,6 +1,6 @@
 def calculate(file_name):
   with open(file_name) as file:
-    lines = [line.rstrip() for line in lines]
+    lines = [line.rstrip() for line in file]
 
 calculate("example_input.txt")
 # calculate("input.txt")
